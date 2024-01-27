5ec6068eb53a804c5bfa586915e0075475af9010
Pasha <pashaie@gmail.comm>
2024-01-27 20:11:57 +0330

    drug list

--- screen/DrugListScreen.tsx
+++ screen/DrugListScreen.tsx
@@ -1,20 +1,42 @@
-import { useEffect } from 'react';
-import { View, Text, Button } from 'react-native'
+import { useEffect, useState } from 'react';
+import { View, Text, Button, SafeAreaView, FlatList, StyleSheet } from 'react-native'
 import { supabase } from '../api';
 export default function DrugListScreen({ navigation }: any) {
+    const [drugList, setDrugList] = useState<any[]>([]);
     const goBack = () => {
         navigation.goBack();
     }
     useEffect(() => {
         (async () => {
             const resp = await supabase.from('Drug').select("*").limit(10);
-            console.log(resp)
+            if (resp.data)
+                setDrugList(resp.data)
         })();
     }, [])
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>DrugList</Text>
-            <Button onPress={goBack} title='Back'></Button>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <FlatList data={drugList} renderItem={({ item }) => <Text style={styles.item}>
+                    {item.drugGenericFaName}
+                </Text>} keyExtractor={(item) => item.id}>
+
+                </FlatList>
+                <Text>DrugList</Text>
+                <Button onPress={goBack} title='Back'></Button>
+            </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 10
+    },
+    item: {
+        backgroundColor: 'silver',
+        padding: 20,
+        marginVertical: 10,
+        fontSize: 24
+    }
+});
