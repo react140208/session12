1dba2043b119cbcd0020bdda011d99a3a6398307
Pasha <pashaie@gmail.comm>
2024-01-27 19:06:05 +0330

    home

--- screen/HomeScreen.tsx
+++ screen/HomeScreen.tsx
@@ -1,8 +1,12 @@
-import { View, Text } from 'react-native'
-export default function HomeScreen() {
+import { View, Text, Button } from 'react-native'
+export default function HomeScreen(props: any) {
+    const gotoDrugList = () => {
+        props.navigation.push('DrugList')
+    }
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home</Text>
+            <Button onPress={gotoDrugList} title='Drug List'></Button>
         </View>
     )
 }
