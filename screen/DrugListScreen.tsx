17ef164c6f9cfea47ce5174755502d775fe2ccaf
Pasha <pashaie@gmail.comm>
2024-01-27 19:29:46 +0330

    get first 10 drug

--- screen/DrugListScreen.tsx
+++ screen/DrugListScreen.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
 import { View, Text, Button } from 'react-native'
+import { supabase } from '../api';
 export default function DrugListScreen({ navigation }: any) {
     const goBack = () => {
         navigation.goBack();
     }
+    useEffect(() => {
+        (async () => {
+            const resp = await supabase.from('Drug').select("*").limit(10);
+            console.log(resp)
+        })();
+    }, [])
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>DrugList</Text>
