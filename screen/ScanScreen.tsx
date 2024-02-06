8b5e3d0870ede212adaa7684f60c86ce5ab4b313
Pashaie <pashaie@gmail.com>
2024-02-06 18:37:48 +0330

    capture photo with camera

--- screen/ScanScreen.tsx
+++ screen/ScanScreen.tsx
@@ -1,12 +1,42 @@
+import { useEffect, useRef } from 'react';
 import { View, Text, Button } from 'react-native'
+import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera'
 export default function ScanScreen(props: any) {
+
+    const { hasPermission, requestPermission } = useCameraPermission();
+    useEffect(() => {
+        (async () => {
+            const result = await requestPermission();
+        })();
+    }, [])
+    const device = useCameraDevice('back')
+    const camera = useRef<Camera>(null)
+    if (device == null) return <Text>No Camera</Text>
 
     const gotoBack = () => {
         props.navigation.goBack()
     }
+    const capture = async () => {
+        if (!camera || !camera.current) return;
+        try {
+            const photo = await camera.current.takePhoto({
+                flash: 'on' // 'auto' | 'off'
+            });
+            console.log(photo)
+        } catch (e) {
+            console.log('error', e)
+        }
+    }
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home</Text>
+            <Camera
+                photo
+                ref={camera}
+                style={{ flex: 1, height: '100%', width: '100%' }}
+                device={device}
+                isActive={true}
+            />
+            <Button onPress={capture} title='Capture'></Button>
             <Button onPress={gotoBack} title='Go Back'></Button>
         </View>
     )
