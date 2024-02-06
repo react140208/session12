db7843b8474602678a0533960fe69a1de2e1039d
Pashaie <pashaie@gmail.com>
2024-02-06 18:51:02 +0330

    data-matrix scan

--- screen/ScanScreen.tsx
+++ screen/ScanScreen.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { View, Text, Button } from 'react-native'
-import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera'
+import { Camera, useCameraDevice, useCameraPermission, useCodeScanner } from 'react-native-vision-camera'
 export default function ScanScreen(props: any) {
 
     const { hasPermission, requestPermission } = useCameraPermission();
@@ -27,9 +27,16 @@ export default function ScanScreen(props: any) {
             console.log('error', e)
         }
     }
+    const codeScanner = useCodeScanner({
+        codeTypes: ['data-matrix'],
+        onCodeScanned: (codes) => {
+            console.log(`Scanned ${codes.length} codes!`, codes)
+        }
+    })
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Camera
+                codeScanner={codeScanner}
                 photo
                 ref={camera}
                 style={{ flex: 1, height: '100%', width: '100%' }}
